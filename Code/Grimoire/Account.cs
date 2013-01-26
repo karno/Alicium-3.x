349497b0a657a6296fd09e4a62ee9259f622ad34
Alice <marisachar@gmail.com>
2013-01-26 11:59:07 +0900

    Account module

--- Code/Grimoire/Account.cs
+++ Code/Grimoire/Account.cs
@@ -7,17 +7,50 @@ namespace Grimoire
 	public class Account
 	{
 		public static List<Account> All = new List<Account>();
-		string token;
-		public Uri BuildAuthUri()
+		static string token;
+		public static Uri BuildAuthUri()
 		{
 			var res = OAuthUtility.GetRequestToken(Alicium.Consumer_Key,Alicium.Consumer_Secret,"oob");
 			token = res.Token;
 			return OAuthUtility.BuildAuthorizationUri(token);
 		}
-		public Account GetAccessToken(string pin)
+		public static Account GetAccessToken(string pin)
 		{
 			var res = OAuthUtility.GetAccessToken(Alicium.Consumer_Key,Alicium.Consumer_Secret,token,pin);
-			
+			return new Account()
+			{
+				Access_Token = res.Token,
+				Access_Secret = res.TokenSecret,
+				Token = new OAuthTokens(){
+					AccessToken = res.Token,
+					AccessTokenSecret = res.TokenSecret,
+					ConsumerKey = Alicium.Consumer_Key,
+					ConsumerSecret = Alicium.Consumer_Secret
+				},
+				Screen_Name = TwitterAccount.VerifyCredentials(new OAuthTokens(){
+					AccessToken = res.Token,
+					AccessTokenSecret = res.TokenSecret,
+					ConsumerKey = Alicium.Consumer_Key,
+					ConsumerSecret = Alicium.Consumer_Secret
+				}).ResponseObject.ScreenName
+			};
 		}
+		public static Account New(string ScreenName,string AccessToken,string AccessSecret)
+		{
+			return new Account()
+			{
+				Screen_Name = ScreenName,
+				Access_Token = AccessToken,
+				Access_Secret = AccessSecret,
+				Token = new OAuthTokens(){
+					AccessToken = AccessToken,
+					AccessTokenSecret = AccessSecret,
+					ConsumerKey = Alicium.Consumer_Key,
+					ConsumerSecret = Alicium.Consumer_Secret
+				}
+			};
+		}
+		public OAuthTokens Token;
+		public string Screen_Name,Access_Token,Access_Secret;
 	}
 }
